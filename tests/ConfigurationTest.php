858a293b618f11a34555f55652f463d6e4892278
Fatih GÜRSOY <fatihgursoy@ymail.com>
2015-07-25 23:18:03 +0300

    test added for Payu\Configuration

---
+++ tests/ConfigurationTest.php
@@ -0,0 +1,43 @@
+<?php namespace Payu\Test;
+
+use Payu\Configuration;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Configuration
+     */
+    protected  $configuration;
+
+    public function setUp() {
+        $this->configuration = new Configuration('TestMerchantId', 'TestSecretKey', 'TestPaymentEndpointUrl',
+            'TestLoyaltyInquiryEndPointUrl');
+    }
+
+    public function testConstructor() {
+        $this->assertEquals('TestMerchantId', $this->configuration->getMerchantId());
+        $this->assertEquals('TestSecretKey', $this->configuration->getSecretKey());
+        $this->assertEquals('TestPaymentEndpointUrl', $this->configuration->getPaymentEndpointUrl());
+        $this->assertEquals('TestLoyaltyInquiryEndPointUrl', $this->configuration->getLoyaltyInquiryEndPointUrl());
+    }
+
+    public function testSetMerchantId() {
+        $this->configuration->setMerchantId('TestMerchantIdSetter');
+        $this->assertEquals('TestMerchantIdSetter', $this->configuration->getMerchantId());
+    }
+
+    public function testSetSecretKey() {
+        $this->configuration->setSecretKey('TestSecretKeySetter');
+        $this->assertEquals('TestSecretKeySetter', $this->configuration->getSecretKey());
+    }
+
+    public function testSetPaymentEndpointUrl() {
+        $this->configuration->setPaymentEndpointUrl('TestPaymentEndpointUrlSetter');
+        $this->assertEquals('TestPaymentEndpointUrlSetter', $this->configuration->getPaymentEndpointUrl());
+    }
+
+    public function testSetLoyaltyInquiryEndPointUrl() {
+        $this->configuration->setLoyaltyInquiryEndPointUrl('TestLoyaltyInquiryEndPointUrlSetter');
+        $this->assertEquals('TestLoyaltyInquiryEndPointUrlSetter', $this->configuration->getLoyaltyInquiryEndPointUrl());
+    }
+}
