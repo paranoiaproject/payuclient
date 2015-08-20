ad4bee62c8fad39c36708ab0298b1d63b6d7bf5d
Fatih GÜRSOY <fatihgursoy@ymail.com>
2015-08-20 11:25:59 +0300

    #5 - 3D Secure desteği eklendi.

--- src/Payu/Serializer/PaymentRequestSerializer.php
+++ src/Payu/Serializer/PaymentRequestSerializer.php
@@ -10,7 +10,7 @@ class PaymentRequestSerializer extends SerializerAbstract
     {
         /** @var $order \Payu\Component\Order */
         $order = $this->request->getOrder();
-
+        
         $data = array(
             'ORDER_REF'                    => $order->getCode(),
             'ORDER_DATE'                   => $order->getDate(),
@@ -18,7 +18,8 @@ class PaymentRequestSerializer extends SerializerAbstract
             'PRICES_CURRENCY'              => $order->getCurrency(),
             'SELECTED_INSTALLMENTS_NUMBER' => $order->getInstallment(),
             'ORDER_TIMEOUT'                => $order->getTimeout(),
-            'CLIENT_IP'                    => $order->getClientIp()
+            'BACK_REF'                     => $this->configuration->getPaymentReturnPointUrl(),
+            'CLIENT_IP'                    => $order->getClientIp(),
         );
 
         if ((float) $order->getLoyaltyAmount() != 0) {
