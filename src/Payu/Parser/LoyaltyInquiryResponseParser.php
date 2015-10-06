8e771a91c03cf79dabfda8a347f9035217fb9568
Onur Şimşek <posta@onursimsek.com>
2015-10-06 16:36:22 +0300

    Puan sorgulamasından status/code dönüşümü yapıldı.

--- src/Payu/Parser/LoyaltyInquiryResponseParser.php
+++ src/Payu/Parser/LoyaltyInquiryResponseParser.php
@@ -17,7 +17,7 @@ class LoyaltyInquiryResponseParser implements ParserInterface
             throw new BadResponseError('Unexpected response received from provider. Response: ' . $rawData);
         }
 
-        $status          = (string) $xml->STATUS;
+        $code          = (string) $xml->STATUS;
         $message         = (string) $xml->MESSAGE;
         $points          = ($xml->POINTS) ? (integer) $xml->POINTS : false;
         $amount          = ($xml->AMOUNT) ? (float) $xml->AMOUNT : false;
@@ -25,16 +25,16 @@ class LoyaltyInquiryResponseParser implements ParserInterface
         $bank            = ($xml->BANK) ? (string) $xml->BANK : false;
         $cardProgramName = ($xml->CARD_PROGRAM_NAME) ? (string) $xml->CARD_PROGRAM_NAME : false;
 
-        switch ($status) {
+        switch ($code) {
             case 'SUCCESS':
-                $code = ResponseAbstract::LOYALITY_STATUS_SUCCESS;
+                $status = ResponseAbstract::LOYALITY_STATUS_SUCCESS;
                 break;
             case 'INPUT_ERROR':
-                $code = ResponseAbstract::LOYALITY_STATUS_INPUT_ERROR;
+                $status = ResponseAbstract::LOYALITY_STATUS_INPUT_ERROR;
                 break;
             case 'FAILED':
             default:
-                $code = ResponseAbstract::LOYALITY_STATUS_FAILED;
+                $status = ResponseAbstract::LOYALITY_STATUS_FAILED;
                 break;
         }
 
