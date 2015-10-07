786ccc73d8170483fe205cfd46726ea220cc66d7
Onur Şimşek <posta@onursimsek.com>
2015-10-07 10:10:49 +0300

    Durumlarda genelleştirme yapıldı.

--- src/Payu/Parser/LoyaltyInquiryResponseParser.php
+++ src/Payu/Parser/LoyaltyInquiryResponseParser.php
@@ -17,7 +17,7 @@ class LoyaltyInquiryResponseParser implements ParserInterface
             throw new BadResponseError('Unexpected response received from provider. Response: ' . $rawData);
         }
 
-        $code          = (string) $xml->STATUS;
+        $code            = (string) $xml->STATUS;
         $message         = (string) $xml->MESSAGE;
         $points          = ($xml->POINTS) ? (integer) $xml->POINTS : false;
         $amount          = ($xml->AMOUNT) ? (float) $xml->AMOUNT : false;
@@ -27,14 +27,14 @@ class LoyaltyInquiryResponseParser implements ParserInterface
 
         switch ($code) {
             case 'SUCCESS':
-                $status = ResponseAbstract::LOYALITY_STATUS_SUCCESS;
+                $status = ResponseAbstract::STATUS_APPROVED;
                 break;
             case 'INPUT_ERROR':
-                $status = ResponseAbstract::LOYALITY_STATUS_INPUT_ERROR;
+                $status = ResponseAbstract::STATUS_INPUT_ERROR;
                 break;
             case 'FAILED':
             default:
-                $status = ResponseAbstract::LOYALITY_STATUS_FAILED;
+                $status = ResponseAbstract::STATUS_FAILED;
                 break;
         }
 
